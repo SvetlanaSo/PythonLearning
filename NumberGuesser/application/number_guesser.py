eada760ca5eed25a51b561bdbf0f78b98b838615
Svetlana Sokolova <SvetlanaSokolova.hog@gmail.com>
2022-12-08 21:22:42 +0100

    Added amounts of attempts

--- NumberGuesser/application/number_guesser.py
+++ NumberGuesser/application/number_guesser.py
@@ -16,14 +16,17 @@ def play_guesser(secret_num):
     print('Мы загадали целое число от 1 до 100. Попробуете угадать? Введите число.')
 
     number_is_guessed = False
+    amount_of_attempts = 0
     while not number_is_guessed:
+        amount_of_attempts += 1
         checking_num = input()
         if not is_valid(checking_num):
             print('А может быть все-таки введем целое число от 1 до 100?')
             continue
         checking_num = int(checking_num)
         if checking_num == secret_num:
-            print('Вы угадали, поздравляем!')
+            print('Вы угадали число, поздравляем!')
+            print('Номер попытки, с которой угадано число:', amount_of_attempts)
             print('Спасибо, что играли в числовую угадайку.')
             number_is_guessed = True
         if checking_num > secret_num:
@@ -32,6 +35,7 @@ def play_guesser(secret_num):
             print('Ваше число меньше загаданного, попробуйте еще разок')
 
 def want_to_play_again():
+    print()
     print('Хотите сыграть еще раз? Введите "да" или "нет".')
     is_answer_correct = False
     while not is_answer_correct:
