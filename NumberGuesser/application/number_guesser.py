e589eeec0588d9d11c732d82df92994806e6f078
Svetlana Sokolova <SvetlanaSokolova.hog@gmail.com>
2022-12-08 19:40:10 +0100

    Added Game itself

--- NumberGuesser/application/number_guesser.py
+++ NumberGuesser/application/number_guesser.py
@@ -9,11 +9,29 @@ def is_valid(number):
         return False
     if not int(number) in range(1, 101):
         return False
-    
     return True
+
 
 if __name__ == "__main__":
     secret_num = rd.randint(1, 100)
     print('Добро пожаловать в числовую угадайку')
+    print('Мы загадали целое число от 1 до 100. Попробуете угадать? Введите число.')
+
+    number_is_guessed = False
+    while not number_is_guessed:
+        checking_num = input()
+        if not is_valid(checking_num):
+            print('А может быть все-таки введем целое число от 1 до 100?')
+            continue
+        checking_num = int(checking_num)
+        if checking_num == secret_num:
+            print('Вы угадали, поздравляем!')
+            print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
+            break
+        if checking_num > secret_num:
+            print('Ваше число больше загаданного, попробуйте еще разок')
+        if checking_num < secret_num:
+            print('Ваше число меньше загаданного, попробуйте еще разок')
+    
 
 
