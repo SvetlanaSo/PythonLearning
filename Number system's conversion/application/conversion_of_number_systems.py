54ffcb4bc6ab321032c9afc7f275299d43a21066
Svetlana Sokolova <SvetlanaSokolova.hog@gmail.com>
2022-12-13 13:25:45 +0100

    Completed program and tests

--- Number system's conversion/application/conversion_of_number_systems.py
+++ Number system's conversion/application/conversion_of_number_systems.py
@@ -3,10 +3,18 @@ def set_values():
     all_values = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 
+def get_valid_answer():
+    answer = input().lower()
+    while not (answer == '' and answer == ''):
+        print('Неверное значение. Введите "да" или "нет".')
+        answer = input().lower()
+    return answer
+
+
 def get_valid_num_of_system():
     num = input()
-    while not num.isdigit():
-        print('Введено неверное значение. Введите число.')
+    while not (num.isdigit() and 1 < int(num) < 37):
+        print('Введено неверное значение. Введите число от 2 до 36 (включительно).')
         num = input()
     return int(num)
 
@@ -23,6 +31,7 @@ def get_valid_number(initial_system):
                 break
     return num
 
+
 def covert_into_10(initial_num, initial_system):
     initial_num = initial_num[::-1]
     converted_num = 0
@@ -31,9 +40,25 @@ def covert_into_10(initial_num, initial_system):
         digit = all_values.find(string_digit)
         converted_num += digit * current_multiplier
         current_multiplier *= initial_system
-    return converted_num
+    return int(converted_num)
 
 
+def convert_into_required_system(number_to_convert, required_system):
+    base = required_system
+    numerator = number_to_convert
+    quotient = numerator
+    array_for_remainders = []
+    while quotient > base:
+        quotient = numerator // base
+        remainder = numerator % base
+        array_for_remainders.append(all_values[remainder])
+        numerator = quotient
+    if quotient > 9:
+        quotient = all_values[quotient]
+    array_for_remainders.reverse()
+    
+    converted_string = str(quotient) + ''.join(array_for_remainders)
+    return converted_string
 
 
 if __name__ == "__main__":
@@ -41,11 +66,18 @@ if __name__ == "__main__":
     set_values()
     while True:
         print('Из какой системы счисления вы хотите осуществить перевод? Укажите число, соответствующее системе счисления.')
-        print('Например, укажите "10", если хотите осуществить перевод из десятичной системы счисления, или "2" - если из двоичной системы счисления. Максимум - "36".')
+        print('Например, укажите "10", если хотите осуществить перевод из десятичной системы счисления, или "2" - если из двоичной системы счисления. Минимум - "2", максимум - "36".')
         initial_system = get_valid_num_of_system()
-        print('Теперь укажите систему счисления, в которую хотите перевести число. Необходимо указать именно число, например "8". Максимум - "36".') 
+        print('Теперь укажите систему счисления, в которую хотите перевести число. Необходимо указать именно число, например "8". Минимум - "2", максимум - "36".') 
         required_system = get_valid_num_of_system()
         print('А теперь введите само число, которое хотите перевести.')
         number = get_valid_number(initial_system)
-        converted_to_10 = covert_into_10(number, initial_system)
-        print(converted_to_10)
+        converted_to_10_num = covert_into_10(number, initial_system)
+        converted_to_required_sys_num = convert_into_required_system(converted_to_10_num, required_system)
+        print(converted_to_required_sys_num)
+        print()
+        print('Хотите перевести еще какое-нибудь число? Введите "да" или "нет".')
+        answer = get_valid_answer()
+        if answer == 'нет':
+            print('Программа завершена.')
+            break
