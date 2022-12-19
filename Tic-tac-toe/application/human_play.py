678df52b33848ff6393baef75166a551dd8ef01b
Svetlana Sokolova <SvetlanaSokolova.hog@gmail.com>
2022-12-19 11:39:44 +0100

    Improved human_play_function

--- Tic-tac-toe/application/human_play.py
+++ Tic-tac-toe/application/human_play.py
@@ -1,7 +1,16 @@
 def get_valid_turn(char_of_player, grid):
     turn = input("Enter a cell's digit where you want to go.")
-    while turn not in '123456789':
-        print('Invalid value entered. Enter a digit from 1 to 9.') 
+    turn_valid = turn in '123456789' \
+        and len(turn) == 1 \
+        and grid[int(turn) - 1] != '[0]' \
+        and grid[int(turn) - 1] != '[X]'
+    while not turn_valid:
+        print('Invalid value entered. Enter a digit from 1 to 9 which points to an empty cell.')
         turn = input("Enter a cell's digit where you want to go.")
+        turn_valid = turn in '123456789' \
+            and len(turn) == 1 \
+            and grid[int(turn) - 1] != '[0]' \
+            and grid[int(turn) - 1] != '[X]'
+    
     grid[int(turn) - 1] = f'[{char_of_player}]'
     return grid
