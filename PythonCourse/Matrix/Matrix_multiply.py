5d7d6f8b2974a9ff8d3ee9015f2e153f1ad735b5
Svetlana Sokolova <SvetlanaSokolova.hog@gmail.com>
2023-01-23 22:54:47 +0100

    added print, corrected params for matrix_multiply

--- PythonCourse/Matrix/Matrix_multiply.py
+++ PythonCourse/Matrix/Matrix_multiply.py
@@ -1,4 +1,4 @@
-def matrix_multiply():
+def matrix_multiply(matrix1, matrix2, rows1, cols2, cols1):
     multi_m = [[0] * cols2 for _ in range(rows1)]
     for i in range(rows1):
         for j in range(cols2):
@@ -15,3 +15,8 @@ rows2, cols2 = [int(i) for i in input().split()]
 matrix2 = [[int(i) for i in input().split()] for _ in range(rows2)]
 
 multi_m = matrix_multiply(matrix1, matrix2, rows1, cols2, cols1)
+
+for i in range(rows1):
+    for j in range(cols2):
+        print(str(multi_m[i][j]).ljust(4), end='')
+    print()
