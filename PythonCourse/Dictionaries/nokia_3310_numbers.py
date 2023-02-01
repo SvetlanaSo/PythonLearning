28961ce5361a1715372a9c4dc8ee4fdd076faa1f
Svetlana Sokolova <SvetlanaSokolova.hog@gmail.com>
2023-02-01 20:59:06 +0100

    updated dictionary for converter

--- PythonCourse/Dictionaries/nokia_3310_numbers.py
+++ PythonCourse/Dictionaries/nokia_3310_numbers.py
@@ -1,10 +1,14 @@
-buttons = {'.,?!:': '1', 'ABC': '2', 'DEF': '3', 'GHI': '4', 'JKL': '5', 
-    'MNO': '6', 'PQRS': '7', 'TUV': '8', 'WXYZ': '9', ' ': '0'
-    }
-
-text = input().upper()
-for el in text:
-    for key, value in buttons.items():
-        if el in key:
-            pos = key.find(el)
-            print(value * (pos + 1), end='')
+buttons = {".":'1', ",":'11', "?":'111', "!":'1111', ":":'11111',
+    "A":'2', "B":'22', "C":'222',
+    "D":'3', "E":'33', "F":'333',
+    "G":'4', "H":'44', "I":'444',
+    "J":'5', "K":'55', "L":'555',
+    "M":'6', "N":'66', "O":'666',
+    "P":'7', "Q":'77', "R":'777', "S": '7777',
+    "T":'8', "U":'88', "V":'888',
+    "W":'9', "X":'99', "Y":'999', "Z": '9999',
+    " ":'0'
+}
+    
+for el in input().upper():
+    print(buttons[el], end='')
